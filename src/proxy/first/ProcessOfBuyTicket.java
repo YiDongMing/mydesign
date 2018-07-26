65b6f1a9040e3296011e280073b45a36e47aa81f
YiDongMing <978325568@qq.com>
2018-07-26 10:27:15 +0800

    更新代理模式

--- src/proxy/first/ProcessOfBuyTicket.java
+++ src/proxy/first/ProcessOfBuyTicket.java
@@ -1,8 +1,10 @@
 package proxy.first;
+import java.lang.reflect.Proxy;
 
 public class ProcessOfBuyTicket {
     public static void main(String[] args) {
         BuyTicket buyTicket = new BuyTicket();
         ProxyOfBuyTicket proxyOfBuyTicket = new ProxyOfBuyTicket(buyTicket);
+        proxyOfBuyTicket.buyTicket();
     }
 }
--- src/proxy/first/ProxyOfBuyTicket.java
+++ src/proxy/first/ProxyOfBuyTicket.java
@@ -1,16 +1,21 @@
 package proxy.first;
 
 public class ProxyOfBuyTicket  implements  IBuyTicket{
-    private BuyTicket buyTicket;
+    //private BuyTicket buyTicket;①
+    private IBuyTicket iBuyTicket;
 
-    public ProxyOfBuyTicket(BuyTicket buyTicket) {
-        this.buyTicket = buyTicket;
+    /*public ProxyOfBuyTicket(BuyTicket buyTicket) {
+        this.buyTicket = buyTicket;①
+    }*/
+    public ProxyOfBuyTicket(IBuyTicket iBuyTicket){
+        this.iBuyTicket = iBuyTicket;
     }
 
     @Override
     public void buyTicket() {
         System.out.println("黄牛拿了你的车票钱和代买费");
-        buyTicket.buyTicket();
+        //buyTicket.buyTicket();①
+        iBuyTicket.buyTicket();
         System.out.println("黄牛把票给你");
     }
 }
